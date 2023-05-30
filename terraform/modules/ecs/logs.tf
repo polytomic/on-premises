077a19ce89e8676dc5acb49be69e73b8dc5fb0c1
Jake Neyer <jake@polytomic.com>
2023-05-30 17:29:07 -0400

    blah

--- terraform/modules/ecs/logs.tf
+++ terraform/modules/ecs/logs.tf
@@ -1,6 +1,5 @@
 module "log_group" {
-  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
-  version = "~> 3.0"
+  source = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
 
   name              = "${var.prefix}-polytomic-logs"
   retention_in_days = var.log_retention_days
