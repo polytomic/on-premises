92c263ed60d9cc452a00b2d33fabd500c4c1dc02
Jake Neyer <jake@polytomic.com>
2023-06-07 10:39:27 -0400

    Split worker and scheduler into separate tasks; fix bucket ACLs

--- terraform/modules/ecs/ecs-tasks.tf
+++ terraform/modules/ecs/ecs-tasks.tf
@@ -103,6 +103,41 @@ resource "aws_ecs_task_definition" "sync" {
 
 }
 
+resource "aws_ecs_task_definition" "scheduler" {
+  family = "${var.prefix}-scheduler"
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.polytomic_resource_scheduler_cpu
+  memory                   = var.polytomic_resource_scheduler_memory
+
+  task_role_arn      = aws_iam_role.polytomic_ecs_task_role.arn
+  execution_role_arn = aws_iam_role.polytomic_ecs_execution_role.arn
+  tags               = var.tags
+
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  container_definitions = templatefile(
+    "${path.module}/task-definitions/scheduler.json.tftpl", local.environment
+  )
+
+  volume {
+    name = "polytomic"
+
+    efs_volume_configuration {
+      file_system_id          = module.efs.id
+      root_directory          = "/"
+      transit_encryption      = "ENABLED"
+      transit_encryption_port = 2999
+    }
+  }
+
+}
+
 resource "aws_ecs_service" "web" {
   name            = "${var.prefix}-web"
   cluster         = var.ecs_cluster_name == "" ? module.ecs[0].cluster_arn : data.aws_ecs_cluster.cluster[0].arn
@@ -171,3 +206,24 @@ resource "aws_ecs_service" "sync" {
   }
 }
 
+
+
+resource "aws_ecs_service" "scheduler" {
+  name            = "${var.prefix}-scheduler"
+  cluster         = var.ecs_cluster_name == "" ? module.ecs[0].cluster_arn : data.aws_ecs_cluster.cluster[0].arn
+  task_definition = aws_ecs_task_definition.scheduler.arn
+  desired_count   = 1
+
+  enable_execute_command = true
+  platform_version       = "1.4.0"
+
+  launch_type = "FARGATE"
+  tags        = var.tags
+
+
+  network_configuration {
+    subnets          = var.vpc_id == "" ? module.vpc[0].private_subnets : var.private_subnet_ids
+    assign_public_ip = false
+    security_groups  = concat(var.additional_ecs_security_groups, [module.fargate_sg.security_group_id])
+  }
+}
